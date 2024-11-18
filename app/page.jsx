a1697fdc33fc0d69f9d21c1a7b3bce31034ba07f
Thomas Price <thomasjprice2@gmail.com>
2024-11-18 18:13:42 +0000

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>What's poppin' near me?</h1>
+    </main>
+  );
+}
---
+++ components/shared/Navbar.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { Github } from "lucide-react"
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center w-full container py-4">
+      <div>
+        <Link href={'/'}>
+          <Image src='/logo.png' width={177.16} height={50} className="max-h-[50px] object-contain" alt="Poppin' Logo" />
+        </Link>
+      </div>
+
+      <div>
+        <Link href='https://github.com' target="_blank">
+          <Button variant='outline'>
+            GitHub
+          </Button>
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
