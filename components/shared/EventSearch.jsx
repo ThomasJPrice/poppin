ffd5009898a96696ed82f92ad1593759033a2047
Thomas Price <thomasjprice2@gmail.com>
2024-11-22 18:22:06 +0000

    initial loda

--- components/shared/EventSearch.jsx
+++ components/shared/EventSearch.jsx
@@ -1,32 +1,39 @@
-'use client'
+'use client';
 
-import Image from "next/image"
-import { useState } from "react"
-import SearchFilters from "./SearchFilters"
-import { searchEvents } from "@/utils/server"
-import EventCard from "./EventCard"
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import SearchFilters from './SearchFilters';
+import { searchEvents } from '@/utils/server';
+import EventCard from './EventCard';
 
 const EventSearch = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [events, setEvents] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [events, setEvents] = useState(null);
 
   const [filters, setFilters] = useState({
     search: '',
     location: '',
     category: '',
-    date: ''
-  })
+    date: '',
+  });
+
+  useEffect(() => {
+    async function fetchInitialEvents() {
+      setIsLoading(true);
+      const res = await searchEvents(filters);
+      setEvents(res);
+      setIsLoading(false);
+    }
+
+    fetchInitialEvents();
+  }, []);
 
   async function handleSearch() {
-    setIsLoading(true)
-
-    const res = await searchEvents(filters)
-
-    setEvents(res)
-
-    setIsLoading(false)
+    setIsLoading(true);
+    const res = await searchEvents(filters);
+    setEvents(res);
+    setIsLoading(false);
   }
-
 
   return (
     <section id="search" className="w-full">
@@ -35,22 +42,30 @@ const EventSearch = () => {
       <div className={`${isLoading ? 'flex justify-center w-full' : ''} mt-8`}>
         {isLoading ? (
           <div className="aspect-square w-[100px] p-4 bounce-container">
-            <Image src="/icon.png" alt="Loading..." width={100} height={100} className="bouncing-image" />
+            <Image
+              src="/icon.png"
+              alt="Loading..."
+              width={100}
+              height={100}
+              className="bouncing-image"
+            />
           </div>
         ) : (
           <>
-            {events ? (
+            {events && events.length > 0 ? (
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
                 {events.map((event, index) => (
-                <EventCard event={event} key={event + index} />
+                  <EventCard event={event} key={`${event.id}-${index}`} />
                 ))}
               </div>
-            ) : <p className="text-center">Looks like there are no events!</p>}
+            ) : (
+              <p className="text-center">Looks like there are no events!</p>
+            )}
           </>
         )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EventSearch
+export default EventSearch;
